3b29233f8672c174d31e3ee0712c142899664eca
Holy-Knight <vnycall74@naver.com>
2017-06-03 16:12:52 +0900

    자동차 조작

--- Assets/Scripts/Moon/CarController.cs
+++ Assets/Scripts/Moon/CarController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+    public float speed = 1.0f;
+
     public FauxGravityAttractor attractor;
     private Transform myTransform;
 
@@ -22,12 +24,21 @@ public class CarController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Move();
 	}
 
     void FixedUpdate()
     {
         if (gravityZone)
             gravityZone.Attract(myTransform);
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.left * h * speed * Time.deltaTime);
+        transform.Translate(Vector3.back * v * speed * Time.deltaTime);
     }
 }
