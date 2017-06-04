bd5b716bc47c7322d5ca8a65666ff961804b2ab6
Holy-Knight <vnycall74@naver.com>
2017-06-04 23:04:08 +0900

    오브젝트 중력 스크립트 추가

---
+++ Assets/Scripts/Moon/GravityObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObject : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+
+    Rigidbody rgd;
+    GravityZone gravityZone;
+
+    // Use this for initialization
+    void Start () {
+        rgd = GetComponent<Rigidbody>();
+
+        rgd.constraints = RigidbodyConstraints.FreezeRotation;
+
+        myTransform = transform;
+        gravityZone = GameObject.Find("MoonGround").GetComponent<GravityZone>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (gravityZone)
+            gravityZone.Attract(myTransform);
+    }
+}
