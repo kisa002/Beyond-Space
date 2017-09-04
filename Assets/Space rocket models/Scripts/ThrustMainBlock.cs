3bc20e02c96b8800fdc2a21da01d77ea844f9fb1
kisa002 <vnycall74@naver.com>
2017-09-04 21:05:26 +0900

    구매 에셋 임포트

---
+++ Assets/Space rocket models/Scripts/ThrustMainBlock.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrustMainBlock : MonoBehaviour {
+
+	public float thrust = 50;
+	public Rigidbody rocket;
+	public bool EngineOn;
+	//public AudioClip engine;
+	//AudioSource audio;
+
+
+
+
+	void Start () 
+	{
+		rocket = GetComponent<Rigidbody> ();
+		EngineOn = false;
+		//audio = GetComponent<AudioSource>();
+		}
+	
+
+	void Update()
+	{
+		EngineStart ();
+		if (EngineOn == true) 
+		rocket.AddRelativeForce (0, thrust, 0, ForceMode.Force);
+
+
+	}
+	private void EngineStart()
+	{
+		
+		if (Input.GetKey ("up")) {
+			EngineOn = true;
+			//audio.PlayOneShot (engine, 0.5F);
+		
+		}
+	}
+
+}
